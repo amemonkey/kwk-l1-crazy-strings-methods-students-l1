c0738866b8500b36af772c1f7e743d2b9f76f8c6
amemonkey <ameliafunaki789@gmail.com>
2018-06-07 14:55:24 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,5 +1,16 @@
 # write your method here
 
-def crazy_strings(a,b)
-
+def crazy_strings(my_name, soda_flavor)
+my_name = my_name.reverse.upcase
+soda_flavor = soda_flavor.gsub!("s", "z").swapcase
+puts "#{my_name}.#{soda_flavor}"
 end
+
+crazy_strings("Ame", "strawberry Fanta")
+
+
+
+
+
+
+# sentence.downcase.gsub!("a","o").capitalize
